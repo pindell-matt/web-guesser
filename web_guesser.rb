2190d46d0b5e3730859ae14ca7e195a802d631bb
Matt Pindell <pindell.m@gmail.com>
2016-03-12 12:43:04 -0700

    Update web_guesser.rb

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,26 +1,59 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-Number = rand(101)
+SECRET_NUMBER = rand(100)
 
 def check_guess(guess)
-  if guess.to_i > Number + 5
-    "Way too high!"
-  elsif guess.to_i > Number
-    "Too high!"
-  elsif guess.to_i < Number - 5
-    "Way too low!"
-  elsif guess.to_i < Number
-    "Too low!"
+  guess = guess.to_i
+  unless guess == SECRET_NUMBER
+    guess > SECRET_NUMBER ? high_guess(guess) : low_guess(guess)
   else
-    "NAILED IT. The Secret Number is #{Number}"
+    congrats_message
+  end
+end
+
+def cheat_mode_activated?
+  params["cheat"]
+end
+
+def win?(message)
+  message == congrats_message
+end
+
+def show_secret?(message)
+  if win?(message) || cheat_mode_activated?
+    "The SECRET NUMBER is #{SECRET_NUMBER}"
+  end
+end
+
+def congrats_message
+  "You got it right!"
+end
+
+def low_guess(guess)
+  guess < (SECRET_NUMBER - 5) ? "Way Too Low!" : "Too Low!"
+end
+
+def high_guess(guess)
+  guess > (SECRET_NUMBER + 5) ? "Way Too High!" : "Too High!"
+end
+
+def color_match(message)
+  unless win?(message)
+    message.include?("Way") ? "#E63838" : "#FF6666"
+  else
+    "#3EC5A0"
   end
 end
 
 get '/' do
-  guess = params["guess"]
-  message = check_guess(guess.to_i)
-  erb :index, :locals => {:number => Number,
-                          :message => message
-                         }
+  guess      = params["guess"]
+  message    = check_guess(guess)
+  background = color_match(message)
+  secret     = show_secret?(message)
+  erb :index, locals: {
+                        message: message,
+                        secret: secret,
+                        background: background
+                      }
 end
